acb1343c3c0a096dba02fdfc26245781193ae225
Vijay Nimar <vijaynimar8@gmail.com>
2025-07-21 01:09:42 +0530

    Update userModel.js

--- userModel.js
+++ userModel.js
@@ -1,17 +1,16 @@
-import {Schema,model} from "mongoose"
-const userSchema=new Schema({
-    phone:{type:Number,required:true,unique:true},
-    name:{type:String},
-    city:{type:String},
+import { Schema, model } from "mongoose";
 
-    selectedActivity:{type:String},
-     deletedAt: {
-      type: Date, 
-      default: null,
-    },
-    joined:{type:Boolean,default:false},
-    createdAt:{type:Date,default:Date.now},
-    deletedAt:{type:String}
-})
-const user=model("user",userSchema)
-export {user}
+const userSchema = new Schema({
+  phone: { type: Number, required: true, unique: true },
+  name: { type: String },
+  city: { type: String },
+  timeSlot: { type: String, default: null },
+  selectedActivity: { type: String, default: null },
+  activityOption: { type: String, default: null },
+  joined: { type: Boolean, default: false },
+  deletedAt: { type: Date, default: null },
+  createdAt: { type: Date, default: Date.now }
+});
+
+const user = model("user", userSchema);
+export { user };
